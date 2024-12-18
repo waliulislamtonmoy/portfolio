74ec7bf1577d12decdbc7f6b0583efd06673d550
waliulislamtonmoy <tonmoykhandiu@gmail.com>
2024-12-19 01:20:50 +0600

    update 4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 3000, // Bind to the environment variable PORT
+    host: true, // Allows listening on 0.0.0.0
+  },
+});
